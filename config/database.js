c7689314f2d81d76542ea2d078c02b331d90b46b
András Ács <acsandras@gmail.com>
2018-11-28 16:09:03 +0100

    Added GitIgnore

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost/local' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://localhost:32768/local' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
